4cab91537cc1b934641a145440aee0bd8bccad5d
angeliquejw <hello@angeliqueweger.com>
2015-04-13 01:15:17 -0400

    Added article layout

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
+Time.zone = "EST"
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
@@ -12,7 +12,7 @@ activate :blog do |blog|
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  blog.layout = "article"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
